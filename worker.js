9b8b0d48b08160f7c303d8c47874ff8b72c5266f
Tony Lee <tonydlee@Mac.lan>
2026-04-23 19:43:15 -0400

    feat(feed): sort dropdown (date/podcast/tickers/material)

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const BACKEND_URL = 'https://equity-analyzer-backend.onrender.com';
-const BUILD_VERSION = '2026-04-23T20';
+const BUILD_VERSION = '2026-04-23T21';
 
 export default {
   async fetch(request, env) {
